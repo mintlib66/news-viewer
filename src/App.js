26e1b6040de3254b849d69a4720afe5e3d2970c5
hy <mintlib66@gmail.com>
2022-10-20 18:25:40 +0900

    뉴스 api 연결

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
+import NewsItem from './components/NewsItem'
 
 function App() {
+  const [data, setData] = useState(null)
+
+  const API_KEY = process.env.REACT_APP_NEWS_API_KEY
+
+  const onClick = async () => {
+    try {
+      axios
+        .get(
+          `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+        )
+        .then(res => {
+          setData(res.data)
+        })
+    } catch (e) {
+      console.log('error: ' + e)
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          row={15}
+          col={10}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        ></textarea>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
